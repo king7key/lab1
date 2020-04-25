e14702c852cd0b767ede36e6e739c33dce9db035
king7key <1264474666@qq.com>
2020-04-25 15:20:35 +0800

    组员小袁update and commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] argv){
         System.out.print("my first lab" );
+        //小袁修改内容
+        //提交至本地仓库并推送到远程仓库
+        System.out.print("Hello World！" );
     }
 }
