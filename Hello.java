361e1423f32b2821b8dd9a5f21da3e201c0a2f41
king7key <1264474666@qq.com>
2020-04-25 21:34:27 +0800

    第一次 组员小袁提交项目

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] argv){
         System.out.print("my first lab" );
+
         //小袁修改内容
         //提交至本地仓库并推送到远程仓库
         System.out.print("Hello World！" );
